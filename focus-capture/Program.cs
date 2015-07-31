feb4644e26bfbe0e49aa818bd256b68ba851aa06
vevix <hello@vevix.net>
2015-07-31 19:42:40 +1000

    initial commit

---
+++ focus-capture/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace focus_capture {
+    class Program {
+        static void Main(string[] args) {
+            // false === full screen screenshot
+            if (Convert.ToBoolean(args[0]) == false) {
+                Screen.Capture(
+                    System.Windows.Forms.Screen.PrimaryScreen.Bounds.X,
+                    System.Windows.Forms.Screen.PrimaryScreen.Bounds.Y,
+                    System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width,
+                    System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height,
+                    args[1]
+                );
+            } else {
+                // crop, or window screenshot
+
+            }
+        }
+    }
+}
---
+++ focus-capture/Screen.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace focus_capture {
+    public static class Screen {
+        public static void Capture(int x, int y, int width, int height, string fileName) {
+            using (var bitmap = new Bitmap(width, height)) {
+                using (var gfx = Graphics.FromImage(bitmap)) {
+                    gfx.CopyFromScreen(x, y, 0, 0, bitmap.Size, CopyPixelOperation.SourceCopy);
+                }
+                var file = Path.GetTempPath() + fileName;
+                bitmap.Save(file, ImageFormat.Png);
+            }
+        }
+    }
+}
